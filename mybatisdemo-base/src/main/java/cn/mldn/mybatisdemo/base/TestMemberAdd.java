9e3c6ea028615b36330f6d6c2ec1092fed1d421a
'lans' <'nalnus@qq.com'>
2017-05-16 15:26:24 +0800

    自定义的MyBatisSessionFactory工具类

--- mybatisdemo-base/src/main/java/cn/mldn/mybatisdemo/base/TestMemberAdd.java
+++ mybatisdemo-base/src/main/java/cn/mldn/mybatisdemo/base/TestMemberAdd.java
@@ -11,6 +11,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
 import cn.mldn.mybatisdemo.vo.Member;
+import cn.mldn.util.MyBatisSessionFactory;
 
 public class TestMemberAdd {
 
@@ -18,12 +19,10 @@ public class TestMemberAdd {
 	public void testInsert() {
 		try {
 			// 1、获取mybatis的核心配置文件，取得了此文件就表示可以取得连接和映射文件信息
-			InputStream inputStream = Resources
-			.getResourceAsStream("mybatis/mybatis.cfg.xml");
+			InputStream inputStream = Resources.getResourceAsStream("mybatis/mybatis.cfg.xml");
 			// 2、获取SqlSessionFactory程序类对象，通过此类对象可以取得MyBatis的Session对象
 			// 3、需要根据mybatis.cfg.xml文件的内容才可以创建SqlSessionFactory工厂类
-			SqlSessionFactory factory = new SqlSessionFactoryBuilder()
-			.build(inputStream);
+			SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
 			// 4、创建Session对象
 			SqlSession session = factory.openSession(); // 有了此对象后才可以进行数据操作
 			// 5、实现数据保存
@@ -35,16 +34,28 @@ public class TestMemberAdd {
 			vo.setSalary(1.1);
 			vo.setNote("是个好人。");
 			// 6、进行数据保存，第一个参数为命名空间的名称，第二个参数是VO类对象；
-			System.out.println(
-			session.insert("cn.mldn.mapping.MemberNS.doCreate", vo));
+			System.out.println(session.insert("cn.mldn.mapping.MemberNS.doCreate", vo));
 			// 7、要进行事务的提交处理
 			session.commit(); // 默认事务是开启的，所以必须自己提交事务
 			// 8、关闭连接
 			session.close();
 			inputStream.close();
-			} catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-			}
+		}
 	}
 
+	@Test
+	public void testAdd() {
+		Member vo = new Member();
+		vo.setMid("mldn - " + new Random().nextInt(1000));
+		vo.setName("测试名字");
+		vo.setAge(19);
+		vo.setBirthday(new Date());
+		vo.setSalary(1.1);
+		vo.setNote("是个好人。");
+		System.out.println(MyBatisSessionFactory.getSession().insert("cn.mldn.mapping.MemberNS.doCreate", vo));
+		MyBatisSessionFactory.getSession().commit();
+		MyBatisSessionFactory.close();
+	}
 }
