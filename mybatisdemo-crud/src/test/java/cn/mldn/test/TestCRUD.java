3b120ab059754dd8372025e9026be2da7c52ffa2
'lans' <'nalnus@qq.com'>
2017-05-17 09:18:37 +0800

    CRUD:数据增加操作 News.xml:namespace，insert语句 mybatis.cfg.xml:别名设置，映射路径设置 日志log4j.properties文件夹提升为源文件夹

---
+++ mybatisdemo-crud/src/test/java/cn/mldn/test/TestCRUD.java
@@ -0,0 +1,23 @@
+package cn.mldn.test;
+
+import org.junit.Test;
+
+import cn.mldn.util.MyBatisSessionFactory;
+import cn.mldn.vo.News;
+import junit.framework.TestCase;
+
+public class TestCRUD {
+
+	@Test
+	public void testInsert() {
+		News vo = new News();
+		vo.setTitle("user");
+		vo.setNote("shiro");
+		int count = MyBatisSessionFactory.getSession().insert("cn.mldn.mapping.NewsNS.doCreate", vo);
+		MyBatisSessionFactory.getSession().commit();
+		MyBatisSessionFactory.close();
+		System.out.println(count);
+		TestCase.assertTrue(count == 1);
+	}
+
+}
